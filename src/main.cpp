b37ff612e2b24c8d13f79f0d003b7a38b1321671
NasirA1 <pusarlai@msn.com>
2023-10-21 18:59:53 +0100

    fixed potential overflow

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char* argv[])
 {    
-    const auto joined = string_helper::join(argv + 1, static_cast<size_t>(argc - 1));
+    const auto joined = string_helper::join(argv + 1, static_cast<size_t>(argc) - 1);
     std::cout << string_helper::trim_all(joined) << std::endl;
 
     std::cout << std::endl << argv[0] << " Version: " << trim_VERSION_MAJOR << "." << trim_VERSION_MINOR << std::endl;
