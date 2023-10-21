fa8a06212084d77d4cef231e88a9e5cb8c509186
nasira1 <pusarlai@msn.com>
2023-10-21 18:21:00 +0100

    added version

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <sstream>
 #include "string_helper.h"
 #include <GLFW/glfw3.h>
+#include "trim_config.h"
 
 
 int main(int argc, char* argv[])
 {    
     const auto joined = string_helper::join(argv + 1, static_cast<size_t>(argc - 1));
     std::cout << string_helper::trim_all(joined) << std::endl;
+
+    std::cout << std::endl << argv[0] << " Version: " << trim_VERSION_MAJOR << "." << trim_VERSION_MINOR << std::endl;
 
     GLFWwindow* window;
     if( !glfwInit() )
