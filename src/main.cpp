655c30758066639a2955ff835c7b5bd2a5808cce
nasira1 <pusarlai@msn.com>
2023-10-21 20:02:56 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <sstream>
 #include "string_helper.h"
-#include <GLFW/glfw3.h>
 #include "trim_config.h"
 
 
@@ -11,32 +10,5 @@ int main(int argc, char* argv[])
     std::cout << string_helper::trim_all(joined) << std::endl;
 
     std::cout << std::endl << argv[0] << " Version: " << trim_VERSION_MAJOR << "." << trim_VERSION_MINOR << std::endl;
-
-    GLFWwindow* window;
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
-
-    window = glfwCreateWindow( 300, 300, "Test: trim", NULL, NULL );
-    if (!window)
-    {
-        fprintf( stderr, "Failed to open GLFW window\n" );
-        glfwTerminate();
-        exit( EXIT_FAILURE );
-    }
-
- // Main loop
-    while( !glfwWindowShouldClose(window) )
-    {
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // Terminate GLFW
-    glfwTerminate();
-
     return EXIT_SUCCESS;
 }
