38fd06e4fd5477423a718fa897b5730cc07dad35
nasira1 <pusarlai@msn.com>
2023-10-22 19:03:22 +0100

    conditional compilation

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,10 @@ int main(int argc, char* argv[])
     std::cout << string_helper::trim_all(joined) << std::endl;
 
     std::cout << std::endl << argv[0] << " Version: " << trim_VERSION_MAJOR << "." << trim_VERSION_MINOR << std::endl;
+#ifdef EMULATION
+    std::cout << "Emulation mode is ON.\n";
+#else
+    std::cout << "Emulation mode is OFF.\n";
+#endif
     return EXIT_SUCCESS;
 }
