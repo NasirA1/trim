2f282aa42ae14e62d8f4875471c347bd7bd8e782
nasira1 <pusarlai@msn.com>
2023-10-23 19:25:26 +0100

    find_library example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main(int argc, char* argv[])
+
+TEST(MyTestSuite, MyTestCase1)
 {
-	std::cout << "hello" << std::endl;
-	return EXIT_SUCCESS;
+	EXPECT_TRUE(1 == 1);
 }
 
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
