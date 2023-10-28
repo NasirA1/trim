c9f0a9cb144153df3399bd10f383df293539c355
NasirA1 <pusarlai@msn.com>
2023-10-29 00:37:05 +0100

    added conan recipe

--- conanfile.py
+++ conanfile.py
@@ -11,23 +11,20 @@ class TrimConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
-    exports_sources = "include/*", "CMakeLists.txt", "src/*"  # Update according to your library structure
+    exports_sources = "include/*", "CMakeLists.txt", "src/*"
 
     def source(self):
-        # This function is used to retrieve the source code of your library
-        # For this example, let's assume you're using a CMake build system
-        self.run("git clone https://github.com/your_username/your_repository.git")
-        self.run("cd your_repository && git checkout tags/v1.0.0")
+        self.run("git clone https://github.com/NasirA1/trim")
+        self.run("cd trim && git checkout conan_tut2")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="your_repository")
+        cmake.configure(source_folder="trim")
         cmake.build()
 
     def package(self):
-        # Specify the files to be included in the package
-        self.copy("*.h", dst="include", src="your_repository/include")
-        self.copy("*MyLibrary.a", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="trim/include")
+        self.copy("*trim.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["MyLibrary"]
+        self.cpp_info.libs = ["trim"]
