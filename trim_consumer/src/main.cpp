7be6cfa7bea921435488a0a0da82bc1dacdbdf9a
NasirA1 <pusarlai@msn.com>
2023-10-29 01:42:45 +0100

    added consumer code

---
+++ trim_consumer/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "arithmetic.h"
+#include "string_helper.h"
+
+int main(int argc, char* argv[])
+{    
+    std::cout << "Hello from trim consumer!\n";
+    std::cout << calc::add(1, 2) << std::endl;
+    std::cout << util::replicate("hello", 3) << std::endl;
+    return EXIT_SUCCESS;
+}
