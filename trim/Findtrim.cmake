158b0a286cb53e9776d52c13b7c8c197ed31cc62
Nasir Ahmad <nasir.ahmad@cubic.com>
2023-11-01 09:15:42 +0000

    wip

--- trim/Findtrim.cmake
+++ trim/Findtrim.cmake
@@ -6,17 +6,12 @@ set(trim_COMPONENTS ai calc util)
 # Set the components as imported targets
 foreach(component ${trim_COMPONENTS})
     add_library(trim::${component} INTERFACE IMPORTED)
-#    target_include_directories(trim::${component} INTERFACE ${CONAN_TRIM_ROOT}/include/${component})
-#    if(NOT ${component} STREQUAL "calc")  ## calc is header-only
-#        target_link_libraries(trim::${component} INTERFACE ${component})
-#    endif()
     set_target_properties(trim::${component} PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${CONAN_TRIM_ROOT}/include/${component}
     )
     if(NOT ${component} STREQUAL "calc")  ## calc is header-only
         set_target_properties(trim::${component} PROPERTIES
-            #IMPORTED_LOCATION ${CONAN_TRIM_ROOT}/lib/${component}
-            IMPORTED_LOCATION ${component}
+            INTERFACE_LINK_LIBRARIES ${component}
         )
     endif()
 endforeach()
